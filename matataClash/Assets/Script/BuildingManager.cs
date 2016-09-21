b9fa49618846828cebae87ef50602008488ec3fb
leonardaus <firdausekarizqi@gmail.com>
2016-09-21 13:18:29 +0700

    \^_^/

--- matataClash/Assets/Script/BuildingManager.cs
+++ matataClash/Assets/Script/BuildingManager.cs
@@ -11,22 +11,39 @@ public class BuildingLimit {
 	public int maxGoldMine;
 	public int maxCamp;
 	public int maxWall; 	
+	public int maxManaGenerator;
 }
-public class BuildingManager : MonoBehaviour {
-
-	public static BuildingManager Instance; 
-	public BuildingLimit buildingLimit;
-    void Awake()
-    {
-        if (!Instance) Instance = this;
-    }
-
-	public List<GameObject> buildingList = new List<GameObject>();
-
+[Serializable]
+public class BuildButton {
 	public Button buildBarrackButton;
 	public Button buildGoldMineButton;
 	public Button buildCampButton;
+	public Button buildWallButton;
+	public Button buildManaGeneratorButton;
+}
+public class BuildingManager : MonoBehaviour {
+	public static BuildingManager Instance; 
+	public BuildingLimit buildingLimit;
+	public BuildButton buildButton;
+	public List<GameObject> buildingList = new List<GameObject>();
+	
+    void Awake()
+    {
+        if (!Instance) Instance = this;
+	}
 
+	void Start(){
+		//if(SceneManager.Instance.isCombatMap){
+			Transform buildPanel = MapManager.Instance.gameObject.transform.GetChild(4).GetChild(1).GetChild(0);
+			buildButton.buildBarrackButton = buildPanel.GetChild(2).GetComponent<Button>();
+			buildButton.buildGoldMineButton = buildPanel.GetChild(0).GetComponent<Button>();
+			buildButton.buildCampButton = buildPanel.GetChild(3).GetComponent<Button>();
+			buildButton.buildWallButton = buildPanel.GetChild(4).GetComponent<Button>();
+			buildButton.buildManaGeneratorButton = buildPanel.GetChild(1).GetComponent<Button>();
+		//}
+
+	}
+	
 	public void addBuilding (GameObject newBuilding){
 		buildingList.Add(newBuilding);
 
@@ -36,18 +53,23 @@ public class BuildingManager : MonoBehaviour {
 		switch(newBuilding.GetComponent<BuildingScript>().buildingType){
 			case 1:	
 				max = buildingLimit.maxBarrack;
-				b = buildBarrackButton;
+				b = buildButton.buildBarrackButton;
 				break;
 			case 2:	
 				max = buildingLimit.maxCamp;
-				b = buildCampButton;
+				b = buildButton.buildCampButton;
 				break;
 			case 3:	
 				max = buildingLimit.maxGoldMine;
-				b = buildGoldMineButton;
+				b = buildButton.buildGoldMineButton;
 				break;			
 			case 4:	
 				max = buildingLimit.maxWall;
+				b = buildButton.buildWallButton;
+				break;
+			case 5:	
+				max = buildingLimit.maxManaGenerator;
+				b = buildButton.buildManaGeneratorButton;
 				break;
 		}
 		foreach (GameObject go in buildingList) {		
@@ -61,35 +83,6 @@ public class BuildingManager : MonoBehaviour {
 				b.gameObject.SetActive(false);
 
 
-	}
-/*
-	public bool CheckIsBuildingMax (int buildingType){
-		int max = 0;
-		int cur = 0;
-		switch(buildingType){
-			case 0:	
-				max = buildingLimit.maxBarrack;
-				break;
-			case 1:	
-				max = buildingLimit.maxGoldMine;
-				break;
-			case 2:	
-				max = buildingLimit.maxCamp;
-				break;
-			case 3:	
-				max = buildingLimit.maxWall;
-				break;
-		}
-		foreach (GameObject g in buildingList)
-		{
-			if(g.GetComponent<BuildingScript>().buildingType == buildingType)
-				cur++;
-		}
-		if(cur<max)
-			return true;
-		else
-			return false;
-	}
-*/	
+	}	
 
 }
