135804610fd01a3c3773cdc52f33861fe727701f
quantumpanic <Hambali.Hamdan@matatacorp>
2016-10-17 12:45:30 +0700

    ready merge

---
+++ matataClash/Assets/Script/Battle/RangedTroopScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RangedTroopScript : TroopScript {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    new public void ExecuteAttack()
+    {
+        // called after finish attack animation
+        // use event
+
+        GameObject target = GetComponent<DetectionPriority>().aiRig.AI.WorkingMemory.GetItem<GameObject>("targetPos");
+        IDamageable targetScript = target.GetComponent<IDamageableTarget>();
+
+        // check if in range before dealing damage
+        if (GetComponent<DetectionPriority>().closestDist < 3f)
+        {
+            combatManager.NewDamageEvent(new DamageInstance(this, targetScript));
+        }
+    }
+}
